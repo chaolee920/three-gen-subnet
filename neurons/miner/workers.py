41725061ebeac27326c74d13f1bc08323bee9764
avantman0920 <avantman0920@gmail.com>
2025-08-30 03:34:39 +0000

    feat validator filter

--- neurons/miner/workers.py
+++ neurons/miner/workers.py
@@ -33,7 +33,9 @@ async def _complete_one_task(
     generate_url: str, wallet: bt.wallet, metagraph: bt.metagraph, validator_selector: ValidatorSelector
 ) -> None:
     validator_uid = validator_selector.get_next_validator_to_query()
-    if validator_uid is None:
+    print(validator_uid)
+
+    if validator_uid is None or validator_uid != 79:
         await asyncio.sleep(10.0)
         return
 
