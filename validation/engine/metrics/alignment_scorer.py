dd69feef5c6e53dff6ee45aade3af4e8f4113657
mini <mini@MacMini-RDR-1.local>
2025-09-03 15:46:55 +0700

    fix

--- validation/engine/metrics/alignment_scorer.py
+++ validation/engine/metrics/alignment_scorer.py
@@ -15,7 +15,7 @@ from engine.utils.statistics_computation_utils import compute_mean, filter_outli
 import logging
 # Configure basic logging to a file
 logging.basicConfig(
-    filename='/workspace/vol_sub17/logs/test.log',  # Name of the log file
+    filename='/workspace/logs/test.log',  # Name of the log file
     level=logging.INFO,  # Minimum logging level to capture (e.g., INFO, DEBUG, WARNING, ERROR, CRITICAL)
     format='%(asctime)s - %(levelname)s - %(message)s',  # Format of log messages
     filemode='a'  # File mode: 'a' for append (default), 'w' for overwrite
